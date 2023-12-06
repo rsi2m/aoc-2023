860a6a4de83db345b7861485b97a6494b468574d
rsi2m <rsi2m@yandex.ru>
2023-12-06 23:13:18 +0200

    Day 4-2 done

--- day-four/main.go
+++ day-four/main.go
@@ -14,12 +14,19 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	cardCount := make(map[int]int)
+
 	var answer int
 
 	line, _, err := reader.ReadLine()
 	for err == nil {
 		stringLine := string(line)
-		gameField := strings.Split(strings.Split(stringLine, ":")[1], "|")
+		cardLine := strings.Split(stringLine, ":")
+
+		cardNumber, _ := strconv.Atoi(strings.TrimSpace(strings.Replace(cardLine[0], "Card", "", -1)))
+		cardCount[cardNumber]++
+
+		gameField := strings.Split(cardLine[1], "|")
 		winningNumberField := strings.Split(strings.TrimSpace(gameField[0]), " ")
 		actualNumberField := strings.Split(strings.TrimSpace(gameField[1]), " ")
 
@@ -41,17 +48,39 @@ func main() {
 			actualNumbers:  actualNumbers,
 			winningNumbers: winningNumbers,
 		}
-		fmt.Println("Card:", card, " Score:", card.calculateScore())
-		answer += card.calculateScore()
+
+		fmt.Println("cardNumber:", cardNumber)
+		for i := 0; i < cardCount[cardNumber]; i++ {
+			apply(cardCount, cardNumber, card.winNumbersAmount())
+			answer++
+		}
 		line, _, err = reader.ReadLine()
 	}
 
-	fmt.Println("Anser:", answer)
+	fmt.Println("Answer:", answer)
+}
+
+func apply(cardCount map[int]int, currentCardNumber int, winNumbersAmount int) {
+	adjustedAmount := winNumbersAmount
+	for i := 1; i <= adjustedAmount; i++ {
+		cardCount[currentCardNumber+i]++
+	}
 }
 
 type card struct {
 	actualNumbers  []int
 	winningNumbers []int
+}
+
+func (c card) winNumbersAmount() int {
+	var score int = 0
+	for i := range c.actualNumbers {
+		number := c.actualNumbers[i]
+		if slices.Contains(c.winningNumbers, number) {
+			score++
+		}
+	}
+	return score
 }
 
 func (c card) calculateScore() int {
