b13fd606e808af2ba0b58382cd103a5f3c02491b
rsi2m <rsi2m@yandex.ru>
2023-12-07 13:02:07 +0200

    Day 5-1 done

--- day-five/main.go
+++ day-five/main.go
@@ -9,25 +9,91 @@ import (
 )
 
 func main() {
-	soilConverter := readSoilConverter()
 
-	fmt.Println("Answer:", soilConverter)
+	seeds := parseSeeds()
+	fmt.Println("Seeds:", seeds)
+
+	converters := []string{"soil", "fertilizer", "water", "light", "temperature", "humidity", "location"}
+
+	// for _, converterName := range converters {
+	// 	converterList := parseConverter(converterName)
+	// 	for _, seed := range seeds {
+	// 		result := seed
+	// 		for _, converter := range converterList {
+	// 			result = process(converter, seed)
+	// 			if result != seed {
+	// 				break
+	// 			}
+
+	// 		}
+	// 		fmt.Println(converterName, ":", seed, "->", result)
+	// 	}
+
+	// }
+
+	var answer int = 999999999999999999
+	for _, seed := range seeds {
+		result := seed
+		for _, converterName := range converters {
+			converterList := parseConverter(converterName)
+			for _, converter := range converterList {
+				newResult := process(converter, result)
+				if newResult != result {
+					result = newResult
+					break
+				}
+
+			}
+		}
+		fmt.Println(seed, "->", result)
+		if result < answer {
+			answer = result
+		}
+	}
+	fmt.Println("Answer:", answer)
+
 }
 
-func readSoilConverter() []SoilConverter {
+func process(converter Converter, seed int) int {
+	if seed >= converter.source && seed <= converter.source+converter.rangeNumber {
+		return seed - converter.source + converter.destination
+	} else {
+		return seed
+	}
+}
+
+func parseSeeds() []int {
+	file, _ := os.Open("./input.txt")
+	reader := bufio.NewReader(file)
+	line, _, _ := reader.ReadLine()
+
+	var seedNumbers []int
+
+	for _, seedString := range strings.Split(strings.Replace(string(line), "seeds:", "", -1), " ") {
+		seedNumber, _ := strconv.Atoi(strings.TrimSpace(seedString))
+		if seedNumber > 0 {
+			seedNumbers = append(seedNumbers, seedNumber)
+		}
+	}
+
+	return seedNumbers
+}
+
+func parseConverter(converterName string) []Converter {
 	file, _ := os.Open("./input.txt")
 	reader := bufio.NewReader(file)
 	var err error
 	var line []byte
 	start := false
 
-	var output []SoilConverter
+	converterStartLine := fmt.Sprintf("-to-%s map:", converterName)
+
+	var output []Converter
 
 	var source, destination, rangeNumber int
 	for err == nil {
 		line, _, err = reader.ReadLine()
 		stringLine := string(line)
-		fmt.Println("Parsing:", stringLine)
 
 		if stringLine == "" && start {
 			break
@@ -37,18 +103,18 @@ func readSoilConverter() []SoilConverter {
 			continue
 		}
 
-		if stringLine == "seed-to-soil map:" {
+		if strings.Contains(stringLine, converterStartLine) {
 			start = true
 			continue
 		}
 
 		if start {
 			inputLines := strings.Split(stringLine, " ")
-			source, _ = strconv.Atoi(inputLines[0])
-			destination, _ = strconv.Atoi(inputLines[1])
+			source, _ = strconv.Atoi(inputLines[1])
+			destination, _ = strconv.Atoi(inputLines[0])
 			rangeNumber, _ = strconv.Atoi(inputLines[2])
 
-			output = append(output, SoilConverter{
+			output = append(output, Converter{
 				source:      source,
 				destination: destination,
 				rangeNumber: rangeNumber,
@@ -60,7 +126,7 @@ func readSoilConverter() []SoilConverter {
 
 }
 
-type SoilConverter struct {
+type Converter struct {
 	source      int
 	destination int
 	rangeNumber int
